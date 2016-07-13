13e30970d5d35bdced907badb01caf4c4996ce92
Xavier-Lam <13599838712@hotmail.com>
2016-07-13 19:15:19 +0800

    soft_time_limit

--- tasks.py
+++ tasks.py
@@ -5,6 +5,7 @@ import logging
 import re
 
 from celery import Celery
+from celery.exceptions import SoftTimeLimitExceeded
 from celery.schedules import crontab
 
 import config
@@ -22,7 +23,7 @@ app.conf.CELERYBEAT_SCHEDULE = {
 }
 app.conf.CELERY_REDIRECT_STDOUTS_LEVEL = "DEBUG"
 
-@app.task(bind=True)
+@app.task(bind=True, soft_time_limit=20)
 def run(self, client, user):
     logger = logging.getLogger("shike." + user.uid)
     delay = config.req_break
@@ -59,6 +60,8 @@ def run(self, client, user):
                 
             if collected:
                 delay = config.success_break
+    except SoftTimeLimitExceeded as e:
+        logger.error("SoftTimeLimitExceeded: " + str(e))
     except Exception as e:
         logger.error("An error occured: " + str(e))
         # self.retry(exe=e, countdown=delay)
@@ -67,49 +70,52 @@ def run(self, client, user):
     except Exception as e:
         logger.critical("Critical: " + str(e))
 
-@app.task
+@app.task(soft_time_limit=20)
 def monitor():
-    pattern = r"^\[(?P<level>\w+)\]\s+(?P<name>[^\s]+)\s+(?P<date>\d{2}-\d{2} \d{2}:\d{2}:\d{2})\s+(?P<msg>.+)"
-    now = datetime.now()
-    with open("errlog.txt") as f:
-        lines = f.readlines()
-        records = list(map(lambda o: re.search(pattern, o).groupdict(), lines))
-        for record in records:
-            record["date"] = datetime.strptime("2016-" + record["date"], "%Y-%m-%d %H:%M:%S")
+    try:
+        pattern = r"^\[(?P<level>\w+)\]\s+(?P<name>[^\s]+)\s+(?P<date>\d{2}-\d{2} \d{2}:\d{2}:\d{2})\s+(?P<msg>.+)"
+        now = datetime.now()
+        with open("errlog.txt") as f:
+            lines = f.readlines()
+            records = list(map(lambda o: re.search(pattern, o).groupdict(), lines))
+            for record in records:
+                record["date"] = datetime.strptime("2016-" + record["date"], "%Y-%m-%d %H:%M:%S")
 
-        # 检查过去5分钟的异常数
-        excs = list(filter(lambda o: now - o["date"] < timedelta(minutes=5) and o["level"] != "WARNING", records))
-        num = len(excs)
-        if num > config.alert_num1:
-            send_wechat_msg(config.alert_openid, config.alert_template, "",
-                keyword1="error", keyword2=num, keyword3=excs[-1]["msg"])
-        # 检查关键日志
-        excs = list(filter(lambda o: now - o["date"] < timedelta(minutes=5) and o["level"] == "CRITICAL", records))
-        num = len(excs)
-        if num > 0:
-            send_wechat_msg(config.alert_openid, config.alert_template, "",
-                keyword1="critical", keyword2=num, keyword3=excs[-1]["msg"])
-        # 检查过去30分钟的日志数
-        excs = filter(lambda o: now - o["date"] < timedelta(minutes=30), records)
-        num = len(list(excs))
-        if num > config.alert_num2:
-            send_wechat_msg(config.alert_openid, config.alert_template, "",
-                keyword1="warning", keyword2=num, keyword3=excs[-1]["msg"])
-                
-    with open("debuglog.txt") as f:
-        lines = f.readlines()
-        records = list(map(lambda o: re.search(pattern, o).groupdict(), lines))
-        for record in records:
-            record["date"] = datetime.strptime("2016-" + record["date"], "%Y-%m-%d %H:%M:%S")
-        
-        # 检查过去5分钟的日志数
-        excs = list(filter(lambda o: now - o["date"] < timedelta(minutes=5), records))
-        num = len(excs)
-        if num < 40:
-            # 过去5分钟日志数少于10条
-            msg = excs[-1]["msg"] if excs else "no msgs"
-            send_wechat_msg(config.alert_openid, config.alert_template, "",
-                keyword1="terminal", keyword2=num, keyword3=msg)
+            # 检查过去5分钟的异常数
+            excs = list(filter(lambda o: now - o["date"] < timedelta(minutes=5) and o["level"] != "WARNING", records))
+            num = len(excs)
+            if num > config.alert_num1:
+                send_wechat_msg(config.alert_openid, config.alert_template, "",
+                    keyword1="error", keyword2=num, keyword3=excs[-1]["msg"])
+            # 检查关键日志
+            excs = list(filter(lambda o: now - o["date"] < timedelta(minutes=5) and o["level"] == "CRITICAL", records))
+            num = len(excs)
+            if num > 0:
+                send_wechat_msg(config.alert_openid, config.alert_template, "",
+                    keyword1="critical", keyword2=num, keyword3=excs[-1]["msg"])
+            # 检查过去30分钟的日志数
+            excs = filter(lambda o: now - o["date"] < timedelta(minutes=30), records)
+            num = len(list(excs))
+            if num > config.alert_num2:
+                send_wechat_msg(config.alert_openid, config.alert_template, "",
+                    keyword1="warning", keyword2=num, keyword3=excs[-1]["msg"])
+                    
+        with open("debuglog.txt") as f:
+            lines = f.readlines()
+            records = list(map(lambda o: re.search(pattern, o).groupdict(), lines))
+            for record in records:
+                record["date"] = datetime.strptime("2016-" + record["date"], "%Y-%m-%d %H:%M:%S")
+            
+            # 检查过去5分钟的日志数
+            excs = list(filter(lambda o: now - o["date"] < timedelta(minutes=5), records))
+            num = len(excs)
+            if num < 40:
+                # 过去5分钟日志数少于10条
+                msg = excs[-1]["msg"] if excs else "no msgs"
+                send_wechat_msg(config.alert_openid, config.alert_template, "",
+                    keyword1="terminal", keyword2=num, keyword3=msg)
+    except SoftTimeLimitExceeded as e:
+        logger.error("SoftTimeLimitExceeded: " + str(e))
 
 def send_wechat_msg(openid, template_id, url="", **kwargs):
     """发送微信消息"""
