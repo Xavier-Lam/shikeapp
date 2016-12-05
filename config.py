a0f9ed1423eb8c26800e96690586511a1cc1799f
Xavier-Lam <13599838712@hotmail.com>
2016-12-05 13:53:50 +0800

    版本参数更新

--- config.py
+++ config.py
@@ -6,8 +6,8 @@ success_break = 450
 down_break = 300
 
 # 接口参数配置
-ver = "1.19"
-binding = "64_1"
+ver = "1.23"
+binding = "81_1"
 ua = r"Mozilla/5.0 (iPhone; CPU iPhone OS 9_3_2 like Mac OS X) AppleWebKit/601.1.46 (KHTML, like Gecko) Version/9.0 Mobile/13F69 Safari/601.1"
 
 # 微信参数配置
